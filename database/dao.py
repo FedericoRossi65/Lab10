d512dccafc574f6c2021ca3d62d4e7a8ebaefcbd
Federico <frossimiglio@gmail.com>
2025-11-25 14:25:44 +0100

    Lab 10

--- database/dao.py
+++ database/dao.py
@@ -1,7 +1,121 @@
 from database.DB_connect import DBConnect
+from model.compagnia import Compagnia
+from model.hub import Hub
+from model.spedizione import Spedizione
+from model.tratta import Tratta
+
 
 class DAO:
     """
     Implementare tutte le funzioni necessarie a interrogare il database.
     """
-    # TODO
+    @staticmethod
+    def get_compagnia() -> list[Compagnia]|None:
+        cnx = DBConnect.get_connection()
+        result = []
+
+        if cnx is None:
+            print("❌ Errore di connessione al database.")
+            return None
+
+        cursor = cnx.cursor(dictionary=True)
+        query = """SELECT * FROM compagnia"""
+        try:
+            cursor.execute(query)
+            for row in cursor:
+                compagnia = Compagnia(
+                    id=row["id"],
+                    codice=row["codice"],
+                    nome=row["nome"]
+
+                )
+
+                result.append(compagnia)
+                print(compagnia)
+        except Exception as e:
+            print(f"Errore durante la query get_compagnia: {e}")
+            result = None
+        finally:
+            cursor.close()
+            cnx.close()
+
+        return result
+
+    @staticmethod
+    def get_hub() -> list[Hub] | None:
+        cnx = DBConnect.get_connection()
+        result = []
+
+        if cnx is None:
+            print("❌ Errore di connessione al database.")
+            return None
+
+        cursor = cnx.cursor(dictionary=True)
+        query = """SELECT * FROM hub"""
+        try:
+            cursor.execute(query)
+            for row in cursor:
+                hub = Hub(
+                    id=row["id"],
+                    codice=row["codice"],
+                    nome=row["nome"],
+                    citta = row['citta'],
+                    stato=row['stato'],
+                    latitudine=row['latitudine'],
+                    longitudine=row['longitudine'],
+                    )
+
+                result.append(hub)
+                print(hub)
+        except Exception as e:
+            print(f"Errore durante la query get_hub: {e}")
+            result = None
+        finally:
+            cursor.close()
+            cnx.close()
+
+        return result
+    @staticmethod
+    def get_tratta() -> list[Tratta] | None:
+        cnx = DBConnect.get_connection()
+        result = []
+
+        if cnx is None:
+            print("❌ Errore di connessione al database.")
+            return None
+
+        cursor = cnx.cursor(dictionary=True)
+        query = """SELECT  LEAST(id_hub_origine,id_hub_destinazione) AS h1,
+                            GREATEST(id_hub_origine,id_hub_destinazione) AS h2, 
+                            SUM(valore_merce) AS valore_totale,
+                            COUNT(*) AS n_spedizioni
+                    FROM spedizione
+                    GROUP BY h1,h2"""
+        try:
+            cursor.execute(query)
+            for row in cursor:
+                tratta = Tratta(
+                    h1=row['h1'],
+                    h2 = row['h2'],
+                    valore_totale = row['valore_totale'],
+                    n_spedizioni = row['n_spedizioni']
+
+
+
+                    )
+
+                result.append(tratta)
+                print(tratta)
+        except Exception as e:
+            print(f"Errore durante la query get_tratta: {e}")
+            result = None
+        finally:
+            cursor.close()
+            cnx.close()
+
+        return result
+l = DAO.get_tratta()
+
+
+
+
---
+++ model/tratta.py
@@ -0,0 +1,19 @@
+from model import spedizione
+from dataclasses import dataclass
+@dataclass
+class Tratta:
+    h1 : int
+    h2 : int
+    valore_totale : float
+    n_spedizioni: int
+
+    def __eq__(self, other):
+        return isinstance(other, Tratta) and self.h1 == other.h1 and self.h2 == other.h2
+
+    def __str__(self):
+        return f"Tratta:{self.h1}-{self.h2}"
+
+    def __repr__(self):
+        return f"Tratta:{self.h1}-{self.h2}"
+    def peso_tratta(self)-> float:
+        return self.valore_totale/self.n_spedizioni
